51bf2274776fff96bd3e179310e32b7b424ef31b
John Shin <jshin029@ucr.edu>
2020-04-12 16:12:17 -0700

    added input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { types, selectStyle } from './constants';
+import '../../fonts.css';
+import './Input.css';
+
+function Input(props) {
+  const { type, placeholder, onChange } = props;
+
+  return (
+    <input
+      className="canopy__input"
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+    >
+    </input>
+  )
+}
+
+Input.propTypes = {
+  onChange: PropTypes.func,
+  type: PropTypes.oneOf(types).isRequired,
+}
+
+export { Input };
