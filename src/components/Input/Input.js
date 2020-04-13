569c235f95bf1d8af302b150056465f2460a2f09
John Shin <jshin029@ucr.edu>
2020-04-12 19:35:20 -0700

    updated input component with changes

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { types, selectStyle } from './constants';
+import { types } from './constants';
 import '../../fonts.css';
 import './Input.css';
 
